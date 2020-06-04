915ebbe84068e3ada0c520f96d20293939bf77ca
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2020-06-04 15:42:55 +0630

    Install react-router-dom, ...
    Fix poster hover box-shadow

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -4,7 +4,7 @@ import styles from './Poster.module.css';
 const Poster = props => {
     return (
         <a href="" >
-            <img src="https://image.tmdb.org/t/p/original/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" className={styles.poster}/>
+            <img src="https://image.tmdb.org/t/p/w1280/jPNShaWZMpVF0iQ7j1dvTuZLD20.jpg" className={styles.poster}/>
         </a>
     );
 }
