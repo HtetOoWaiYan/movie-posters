314431652fdda5526bf7c4c2fb9671fdc34a74e4
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2020-06-07 17:39:52 +0630

    Add tooltip on poster hover with title tag

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -3,8 +3,8 @@ import styles from './Poster.module.css';
 
 const Poster = props => {
     return (
-        <a href={`https://image.tmdb.org/t/p/w1280/${props.movie.poster_path}`} >
-            <img src={`https://image.tmdb.org/t/p/w1280/${props.movie.poster_path}`} className={styles.poster}/>
+        <a href={`/poster/${props.movie.id}`} title={`${props.movie.title} (${props.movie.release_date.substring(0, 4)})`}>
+            <img src={`https://image.tmdb.org/t/p/w1280/${props.movie.poster_path}`} alt={props.movie.title} className={styles.poster}/>
         </a>
     );
 }
