7054ab436bc969fe3921f52525331d8cf5ceab36
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2020-06-24 18:25:00 +0630

    Add and style Select compoent at home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import Posters from '../Posters/Posters';
 import styles from './Home.module.css';
-import { Input } from 'antd';
+import { Input, Select } from 'antd';
 const { Search } = Input;
+const { Option } = Select;
 
 const Home = props => {
+    function handleChange(value) {
+        console.log(`selected ${value}`);
+    }
+
     return (
         <div>
-            <Search
-                enterButton
-                size="large"
-                placeholder="Search movies"
-                onSearch={value => console.log(value)}
-                className={styles.search}
-            />
+            <div className={styles.inputs}>
+                <div></div>
+                <Search
+                    enterButton
+                    size="large"
+                    placeholder="Search movies"
+                    onSearch={value => console.log(value)}
+                    className={styles.search}
+                />
+                <Select
+                    value="large"
+                    defaultValue="lucy"
+                    onChange={handleChange}
+                    className={styles.select}
+                >
+                    <Option value="jack">Jack</Option>
+                    <Option value="lucy">Lucy</Option>
+                    <Option value="disabled" disabled>
+                        Disabled
+                    </Option>
+                    <Option value="Yiminghe">yiminghe</Option>
+                </Select>
+            </div>
             <Posters movies={props.movies} />
         </div>
     )
