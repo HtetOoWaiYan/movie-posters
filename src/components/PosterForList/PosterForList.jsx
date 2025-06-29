29192481339ae2ebc7fb0e76e98fe72fc1a69f72
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2025-06-29 08:06:15 -0700

    fix: correct Link component syntax in PosterForList

--- src/components/PosterForList/PosterForList.jsx
+++ src/components/PosterForList/PosterForList.jsx
@@ -4,13 +4,13 @@ import styles from "./PosterForList.module.css";
 
 const PosterForList = React.memo((props) => {
   return (
-    <Link>
+    <Link
       to={`/posters/${props.movie_id}/${props.poster_id}`}
-      state=
-      {{
+      state={{
         fromSearch: props.fromSearch,
         searchQuery: props.searchQuery,
       }}
+      >
       <img
         src={`https://image.tmdb.org/t/p/w500/${props.poster.file_path}`}
         alt="poster"
